fad285fdb3160f80af6344c47eea90d3a595eab2
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-09-29 16:06:31 +0500

    Task1

---
+++ Tyuiu.PupovAA.Sprint1.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.PupovAA.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2.00;
+            double y = 3.00;
+            var res = ds.Calculate (x, y);
+            Assert.AreEqual(1 ,res);
+            
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint1.Task1.V21/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task1.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #0 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич | ИБКСб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данны,  *");
+        Console.WriteLine("* вычисляет результат по формуле (x*y)/(3+y) и печатает его на экране.    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* (x*y)/(3+y)                                                             *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+
+        Console.WriteLine("Введите число x");
+        x=Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите число y");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(ds.Calculate(x, y));
+        Console.ReadLine();
+    }
+    
+}
