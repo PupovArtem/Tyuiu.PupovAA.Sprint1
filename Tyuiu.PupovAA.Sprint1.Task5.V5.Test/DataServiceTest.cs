91c2a2e5e18d49f13522f9d1a44b4cb6b3b5d069
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-01 08:29:41 +0500

    Task5

---
+++ Tyuiu.PupovAA.Sprint1.Task5.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task5.V5.Lib;
+namespace Tyuiu.PupovAA.Sprint1.Task5.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Double x = 32.597;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint1.Task5.V5/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task5.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Преобразование типов и класс Conver                               *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич | ИБКСб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Присвоить целой переменной d первую цифру из дробной части              *");
+        Console.WriteLine("* положительного вещественного числа x                                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("* если x=32.597, то d=5                                                   *");
+        Console.WriteLine("***************************************************************************");
+
+        double x;
+        Console.WriteLine("Введите вещественное число");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.Write("1 цифрой после запятой является - ");
+        int res = Convert.ToInt32(ds.Calculate(x));
+        Console.Write(res);
+        Console.ReadLine();
+
+    }
+}
