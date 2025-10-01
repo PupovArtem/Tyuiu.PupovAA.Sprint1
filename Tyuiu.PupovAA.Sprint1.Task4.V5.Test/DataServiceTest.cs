fb271a5b6bdbb02fdb403a18dff8accb52e65cbd
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-01 07:33:20 +0500

    Task4

---
+++ Tyuiu.PupovAA.Sprint1.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task4.V5.Lib;
+namespace Tyuiu.PupovAA.Sprint1.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Double x = 2;
+            Double y = 4;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.25, res);
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint1.Task4.V5/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task4.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Class Math                                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич | ИБКСб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычесляет по формуле и печатает результат на экране.                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("* Форму                                                                   *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+        Console.WriteLine("Введите число x");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите число y");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Результат вычесления = " + ds.Calculate(x, y));
+        Console.ReadLine();
+
+
+    }
+}
