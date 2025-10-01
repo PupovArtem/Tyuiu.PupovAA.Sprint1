d3c462df48df4cc646689e6a66d5f46fb5b1ef6d
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-01 15:22:38 +0500

    Task7

---
+++ Tyuiu.PupovAA.Sprint1.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PupovAA.Sprint1.Task7.V16.Lib;
+namespace Tyuiu.PupovAA.Sprint1.Task7.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Double x = 1.0;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(1.022, res);
+        }
+    }
+}
